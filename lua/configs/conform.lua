b61f8f40ac7cb34524d2723194e8012560a019fb
say8hi <maks.mir@yahoo.com>
2025-10-02 13:40:21 +0500

    fix(conform): correct config structure for format_on_save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,27 +1,20 @@
 local options = {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        go = { "gofumpt", "goimports-reviser", "golines" },
-        python = { "ruff_format", "ruff_organize_imports" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        json = { "prettier" },
-        html = { "prettier" },
-        css = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        lua = { "stylua" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-    })
-  end,
+  formatters_by_ft = {
+    go = { "gofumpt", "goimports-reviser", "golines" },
+    python = { "ruff_format", "ruff_organize_imports" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    json = { "prettier" },
+    html = { "prettier" },
+    css = { "prettier" },
+    yaml = { "prettier" },
+    markdown = { "prettier" },
+    lua = { "stylua" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
